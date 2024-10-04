dcee0927dadf172e10889211c017df198a2355ed
shankey raheja <shankeyraheja@gmail.com>
2024-10-05 08:03:54 +1300

    sign in fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Route, Routes } from "react-router-dom"
 import { Link, useParams } from "react-router-dom"
 import Home from "./component.jsx/Home"
@@ -11,10 +11,23 @@ import SearchResults from "./component.jsx/SearchResults"
 import Account from "./component.jsx/Account"
 import Post from "./component.jsx/Post"
 import { auth } from "./firebase/config"
+import SignIn from "./component.jsx/SignIn"
+import { onAuthStateChanged, signOut } from "firebase/auth"
 
 function App() {
   console.log("auth", auth.currentUser)
   const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser)
+    })
+  }, [])
+
+  const handleLogOut = async () => {
+    await signOut(auth)
+  }
+
   return (
     <div>
       <header style={{ backgroundColor: "#f8f9fa", height: "100px" }}>
@@ -36,6 +49,9 @@ function App() {
             <li>
               <Link to={"/"}>Home</Link>{" "}
             </li>
+            <li>
+              <Link onClick={handleLogOut}>Log Out</Link>{" "}
+            </li>
           </ul>
         </nav>
       </header>
@@ -44,7 +60,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="searchbar" element={<SearchBar />} />
           <Route path="searchresults" element={<SearchResults />} />
-          <Route path="account" element={<Account />} />
+          <Route
+            path="account"
+            element={user ? <Account /> : <SignIn setUser={setUser} />}
+          />
           <Route path="post" element={<Post />} />
         </Routes>
       </main>
