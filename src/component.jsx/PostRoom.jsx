36322399546fc5d6c398a078aa9899a4be23a819
shankey raheja <shankeyraheja@gmail.com>
2024-11-09 12:24:32 +1300

    fixed upload multiple images to sanity

--- src/component.jsx/PostRoom.jsx
+++ src/component.jsx/PostRoom.jsx
@@ -1,192 +1,194 @@
-import { useState } from 'react'
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import CardHeader from '@mui/material/CardHeader';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
+import { useState } from "react"
+import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
+import Card from "@mui/material/Card"
+import CardContent from "@mui/material/CardContent"
+import Button from "@mui/material/Button"
+import CardHeader from "@mui/material/CardHeader"
+import FormControlLabel from "@mui/material/FormControlLabel"
+import FormControl from "@mui/material/FormControl"
+import FormLabel from "@mui/material/FormLabel"
 import TextField from "@mui/material/TextField"
 import Avatar from "@mui/material/Avatar"
-import { v4 as uuidv4 } from 'uuid';
-import { auth } from '../firebase/config';
-import client from "../sanityClient";
-
+import { v4 as uuidv4 } from "uuid"
+import { auth } from "../firebase/config"
+import client from "../sanityClient"
 
 const PostRoom = () => {
-    const [roomPosting, setRoomPosting] = useState({
-        id: '',
-        area: '',
-        neighborhood: '',
-        description: '',
-        price: '',
-        photo: [],
-        availability: '',
-        amenities: ''
-    })
+  const [roomPosting, setRoomPosting] = useState({
+    id: "",
+    area: "",
+    neighborhood: "",
+    description: "",
+    price: "",
+    photo: [],
+    availability: "",
+    amenities: "",
+  })
 
-    const uploadImageToSanity = async (base64String, fileName = 'image.png') => {
-        try{
-          // const file = Buffer.from(base64String, 'base64')  // Convert base64 string to file const 
-          const response = await client.assets.upload('image', base64String, { 
-            contentType: "png",filename: fileName    // Specify the filename for the uploaded image(passing file to sanity)
-            });
-            return response 
-        } catch (error) { console.error('Image upload failed:', error.message); throw error; }
+  const [photosArray, setPhotoArray] = useState([])
+
+  const uploadImageToSanity = async (base64String, fileName = "image.png") => {
+    try {
+      // const file = Buffer.from(base64String, 'base64')  // Convert base64 string to file const
+      const response = await client.assets.upload("image", base64String, {
+        contentType: "png",
+        filename: fileName, // Specify the filename for the uploaded image(passing file to sanity)
+      })
+      return response
+    } catch (error) {
+      console.error("Image upload failed:", error.message)
+      throw error
+    }
+  }
+  console.log("roomPosting", roomPosting)
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    let images = []
+
+    for (const pic of roomPosting.photo) {
+      const image_upload_response = await uploadImageToSanity(pic)
+      let image = {
+        _key: uuidv4(),
+        _type: "image",
+        asset: {
+          _type: "reference",
+          _ref: image_upload_response._id,
+        },
       }
-
-    const handleSubmit = async (event) => {
-        console.log(roomPosting)
-        event.preventDefault();
-        let images = []
-        roomPosting.photo.map((pic, i) => {
-            const data = async () => {
-                let image_upload_response = await uploadImageToSanity(pic)
-                let image = {
-                    _key:uuidv4(),
-                    _type: "image",
-                    asset: {
-                      _type: "reference",
-                      _ref: image_upload_response._id
-                    }}
-                    images.push(image)
-                    console.log("images", images)
-        if(i === roomPosting.photo.length - 1){
-            client.createOrReplace({_id:uuidv4(), _type:"roomposting",id:auth.currentUser.uid, area:roomPosting.area, neighborhood:roomPosting.neighborhood, price: roomPosting.price, availability:roomPosting.availability,description:roomPosting.description, amenities:roomPosting.amenities, images:images})
-        }
-                    
-            }
-           data()
-        })
-        
+      images.push(image)
     }
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setRoomPosting({ ...roomPosting, [name]: value })
+    // After all images are uploaded, create or replace the document
+    await client.createOrReplace({
+      _id: uuidv4(),
+      _type: "roomposting",
+      id: auth.currentUser.uid,
+      area: roomPosting.area,
+      neighborhood: roomPosting.neighborhood,
+      price: roomPosting.price,
+      availability: roomPosting.availability,
+      description: roomPosting.description,
+      amenities: roomPosting.amenities,
+      images: images,
+    })
+  }
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    setRoomPosting({ ...roomPosting, [name]: value })
+  }
+
+  const addRoomPosting = async (roomPosting) => {
+    try {
+      const response = await fetch("URL", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json ; charset=UTF-8",
+        },
+        body: JSON.stringify(roomPosting),
+      })
+
+      if (response.ok) {
+        const jsonResponse = await response.json()
+        const data = await jsonResponse
+        setRoomPosting(data)
+      }
+    } catch (error) {
+      console.log("Error:", error)
     }
+  }
 
-    const addRoomPosting = async (roomPosting) => {
-        try {
-            const response = await fetch('URL', {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json ; charset=UTF-8'
-                },
-                body: JSON.stringify(roomPosting)
-            });
+  const handleUploadPhoto = (e) => {
+    const fileReaders = []
+    const files = e.target.files
+    if (files.length > 0) {
+      console.log(files)
 
-            if (response.ok) {
-                const jsonResponse = await response.json();
-                const data = await jsonResponse;
-                setRoomPosting(data);
-            }
+      Array.from(files).forEach((file, index) => {
+        const reader = new FileReader()
+        fileReaders.push(reader)
 
-        } catch (error) {
-            console.log('Error:', error)
+        reader.onloadend = () => {
+          setPhotoArray([...photosArray, file])
+
+          // Check if all files have been processed
+          //   if (photosArray.length === files.length) {
+          console.log("photosArray", photosArray)
+          setRoomPosting((prev) => ({
+            ...prev,
+            photo: [...photosArray, file], // Set the state with the array of photos
+          }))
+          //   }
         }
-    };
 
-    // const handleUploadPhoto = (e) => {
-    //     const files = e.target.files
-    //     console.log("files", files)
-    //     if(files){
-    //     files.map(file => {
-    //         reader.readAsDataURL(file)
-    //     })
-    const fileReaders = [];
-    const photosArray = [];  
+        reader.readAsDataURL(file)
+      })
+    }
+  }
 
-    const handleUploadPhoto = (e) => {
-        const files = e.target.files;
-        if (files.length > 0) {
-            console.log(files)
-          
-      
-          Array.from(files).forEach((file, index) => {
-            const reader = new FileReader();
-            fileReaders.push(reader);
-      
-            reader.onloadend = () => {
-              photosArray.push(file);
-              
-              // Check if all files have been processed
-            //   if (photosArray.length === files.length) {
-                setRoomPosting((prev) => ({
-                  ...prev,
-                  photo: photosArray, // Set the state with the array of photos
-                }));
-            //   }
-            };
-      
-            reader.readAsDataURL(file);
-          });
-        }
-      };
-      
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
 
-    return (
-        <Box sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-
-            bgcolor: '#f5f5f5',
-            p: 2,
-        }}>
-            <Card sx={{ width: 550, p: 3 }}>
-                <CardHeader
-                    title={
-                        <Typography variant="h5" color="textPrimary" align="center">
-                            Post a Room
-                        </Typography>
-                    }
-                />
-                <CardContent component="form" >
-                    <FormControl fullWidth margin="normal">
-                        <FormLabel>Area</FormLabel>
-                        <TextField
-                            name='area'
-                            value={roomPosting.area}
-                            onChange={handleInputChange}
-                            variant='outlined'
-                        />
-                    </FormControl>
-                    <FormControl fullWidth margin="normal">
-                        <FormLabel>Neighborhood</FormLabel>
-                        <TextField
-                            name='neighborhood'
-                            value={roomPosting.neighborhood}
-                            onChange={handleInputChange}
-                            variant='outlined'
-                        />
-
-                    </FormControl>
-                    <FormControl fullWidth margin="normal">
-                        <FormLabel>Description</FormLabel>
-                        <TextField
-                            name='description'
-                            value={roomPosting.description}
-                            onChange={handleInputChange}
-                            variant='outlined'
-                            multiline
-                            rows={3}
-                        />
-                    </FormControl>
-                    <FormControl fullWidth margin="normal">
-                        <FormLabel>Price</FormLabel>
-                        <TextField
-                            name='price'
-                            value={roomPosting.price}
-                            onChange={handleInputChange}
-                            variant='outlined'
-                        />
-                    </FormControl>
-                    <FormControl>
-                        <FormLabel>Photos</FormLabel>
-                        {/* {roomPosting.photo && roomPosting.photo[0] ? (
+        bgcolor: "#f5f5f5",
+        p: 2,
+      }}
+    >
+      <Card sx={{ width: 550, p: 3 }}>
+        <CardHeader
+          title={
+            <Typography variant="h5" color="textPrimary" align="center">
+              Post a Room
+            </Typography>
+          }
+        />
+        <CardContent component="form">
+          <FormControl fullWidth margin="normal">
+            <FormLabel>Area</FormLabel>
+            <TextField
+              name="area"
+              value={roomPosting.area}
+              onChange={handleInputChange}
+              variant="outlined"
+            />
+          </FormControl>
+          <FormControl fullWidth margin="normal">
+            <FormLabel>Neighborhood</FormLabel>
+            <TextField
+              name="neighborhood"
+              value={roomPosting.neighborhood}
+              onChange={handleInputChange}
+              variant="outlined"
+            />
+          </FormControl>
+          <FormControl fullWidth margin="normal">
+            <FormLabel>Description</FormLabel>
+            <TextField
+              name="description"
+              value={roomPosting.description}
+              onChange={handleInputChange}
+              variant="outlined"
+              multiline
+              rows={3}
+            />
+          </FormControl>
+          <FormControl fullWidth margin="normal">
+            <FormLabel>Price</FormLabel>
+            <TextField
+              name="price"
+              value={roomPosting.price}
+              onChange={handleInputChange}
+              variant="outlined"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Photos</FormLabel>
+            {/* {roomPosting.photo && roomPosting.photo[0] ? (
                             roomPosting.photo.map((p, i) => {
                                 return (
                                     <img
@@ -199,53 +201,52 @@ const PostRoom = () => {
                         ) : (
                             <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
                         )} */}
-                        {roomPosting.photo && roomPosting.photo[0] &&
-                            roomPosting.photo.map((p, i) => (
-                                    <img
-                                        src={p}
-                                        alt="Uploaded Preview"
-                                        style={{ width: 70, height: 60,  }}
-                                    />
-                                ))
-                        }
-                        <Button variant="outlined" component="label">
-                            Upload Photo
-                            <input
-                                type="file"
-                                hidden
-                                accept="image/*"
-                                onChange={handleUploadPhoto}
-                            />
-                        </Button>
-
-                    </FormControl>
-                    <FormControl fullWidth margin="normal">
-                        <FormLabel>Availability</FormLabel>
-                        <TextField
-                            name='availability'
-                            value={roomPosting.availability}
-                            onChange={handleInputChange}
-                            variant='outlined'
-                        />
-                    </FormControl>
-                    <FormControl fullWidth margin="normal">
-                        <FormLabel>Amenities</FormLabel>
-                        <TextField
-                            name='amenities'
-                            value={roomPosting.amenities}
-                            onChange={handleInputChange}
-                            variant='outlined'
-                            multiline
-                            rows={3}
-                        />
-                    </FormControl>
-                    <Button variant="contained" onClick={handleSubmit}>
-                        Create
-                    </Button>
-                </CardContent>
-            </Card>
-        </Box>
-    )
+            {roomPosting.photo &&
+              roomPosting.photo[0] &&
+              roomPosting.photo.map((p, i) => (
+                <img
+                  src={p}
+                  alt="Uploaded Preview"
+                  style={{ width: 70, height: 60 }}
+                />
+              ))}
+            <Button variant="outlined" component="label">
+              Upload Photo
+              <input
+                type="file"
+                hidden
+                accept="image/*"
+                onChange={handleUploadPhoto}
+              />
+            </Button>
+          </FormControl>
+          <FormControl fullWidth margin="normal">
+            <FormLabel>Availability</FormLabel>
+            <TextField
+              name="availability"
+              value={roomPosting.availability}
+              onChange={handleInputChange}
+              variant="outlined"
+            />
+          </FormControl>
+          <FormControl fullWidth margin="normal">
+            <FormLabel>Amenities</FormLabel>
+            <TextField
+              name="amenities"
+              value={roomPosting.amenities}
+              onChange={handleInputChange}
+              variant="outlined"
+              multiline
+              rows={3}
+            />
+          </FormControl>
+          <Button variant="contained" onClick={handleSubmit}>
+            Create
+          </Button>
+        </CardContent>
+      </Card>
+    </Box>
+  )
 }
 
-export default PostRoom;
+export default PostRoom
