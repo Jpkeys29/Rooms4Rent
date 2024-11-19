8b0b950d22af8fad65d26cb5f85e3d11121d69ff
Jp Keys <jpmusik29@gmail.com>
2024-11-19 07:01:27 -0500

    Rendering Home Postings

--- src/component.jsx/Home.jsx
+++ src/component.jsx/Home.jsx
@@ -1,32 +1,48 @@
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import Post from "./Post";
-import Box from '@mui/material/Box';
-
+import Box from "@mui/material/Box";
+import client from '../sanityClient'
 
 const Home = () => {
-    return (
-        <div style={{height:'100vh'}}>
-            <Box
-                sx={{
-                    backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.4)), url(/banner.jpg)', 
-                    // backgroundImage: 'url(/banner.jpg)',
-                    backgroundPosition: 'center top',
-                    backgroundSize: 'cover',
-                    backgroundRepeat: 'no-repeat',
-                    height: '50%',
-                    padding: 3,
-                    
-                }}
-            >
-                <div>
-                    <SearchBar />
-                </div>
-            </Box>
-                <Post />
+  const [roomPosting, setRoomPosting] = useState([]);
 
+  useEffect(() => {
+    const fetchRoomPosting = async () => {
+      const query = '*[_type == "roomposting"]';
+      const postsGeneral = await client.fetch(query);
+      console.log(postsGeneral);
+      setRoomPosting(postsGeneral);
+    };
+    fetchRoomPosting();
+  }, []);
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <Box
+        sx={{
+          backgroundImage:
+            "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.4)), url(/banner.jpg)",
+          // backgroundImage: 'url(/banner.jpg)',
+          backgroundPosition: "center top",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          height: "50%",
+          padding: 3,
+        }}
+      >
+        <div>
+          <SearchBar />
         </div>
-    )
-}
+      </Box>
+      {roomPosting.map((posts, index) => {
+        return(
+            <Post key={index} postings={posts}/>
+        )
+      })}
+    </div>
+  );
+};
 
 export default Home;
--- src/component.jsx/renderedPosting.jsx
+++ src/component.jsx/renderedPosting.jsx
@@ -1,35 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from "@mui/material/Card"
 import CardContent from "@mui/material/CardContent"
 import Button from "@mui/material/Button"
 import Typography from "@mui/material/Typography"
 import { CardMedia } from '@mui/material'
 import CardHeader from '@mui/material/CardHeader'
+import imageUrlBuilder from '@sanity/image-url'
+import client from '../sanityClient'
+import { auth } from "../firebase/config"
+import CardPosting from './postingCards/Card'
 
-function renderedPosting() {
+function RenderedPosting() {
+    const [postingDetails, setPostingDetails] = useState([])
+    useEffect(() => {
+        const fetchPostingDetails = async () => {
+            let userId = auth?.currentUser?.uid;
+            if (userId) {
+                const postingDetails = await client.fetch();
+                const query = '*[_type == "roomposting" && id == $userId]';
+                const params = { userId: userId }; 
+                const postsByAccount = await client.fetch(query, params)
+                console.log(postsByAccount)
+                setPostingDetails(postsByAccount)
+            }    
+        }
+        fetchPostingDetails();
+    }, [auth, auth?.currentUser])
     return (
-        <Card>
-            <CardHeader title="My Account"
-                titleTypographyProps={{ align: 'center', variant: 'h5' }}
-            />
-            <CardMedia
-            />
-            <CardContent>
-                <Typography>
-
-                </Typography>
-                <Typography>
-
-                </Typography>
-                <Typography>
-
-                </Typography>
-                <Typography>
-
-                </Typography>
-            </CardContent>
-        </Card>
+        <div sx={{
+            display: "flex",
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '80%'
+          }}>
+            {postingDetails.map((p, index) => {
+                return( 
+                    <CardPosting posting={p} key={index} />   
+                )
+            })
+         }       
+        </div>
     )
 }
 
-export default renderedPosting
+export default RenderedPosting
